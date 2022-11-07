d63b82c275442e790fb9acc3e33fc5e8f3025233
Louis-Marie BAER <lm@baermail.fr>
2022-11-07 16:49:27 +0100

    amélioration perf: lire tout les fichiers en mémoire avant d'y chercher

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::io;
 use std::path::Path;
 use std::process::exit;
 
+use vcard_parser::parse_to_vcards_without_errors;
 use vcard_parser::vcard::property::types::PropertyType;
 use vcard_parser::vcard::Vcard;
-use vcard_parser::{parse_to_vcards_without_errors};
 
 use clap::{Args, Parser, Subcommand};
 
@@ -36,24 +36,58 @@ enum Commands {
 fn find(property_show: PropertyType, property_find: PropertyType, value_find: String) {
     let dir_contacts = Path::new("/home/lm/.contacts/default");
     let files = fs::read_dir(dir_contacts).unwrap();
+    let mut all = String::from("");
     for file in files {
-        if let Ok(string) = read_to_string(file.unwrap().path()) {
-            let vcards = parse_to_vcards_without_errors(string.as_str());
-            let vcard = vcards.iter().next().unwrap().clone();
-            let properties = vcard.get_properties_by_type(&property_find);
-            if !properties.is_empty() {
-        let mut property = String::from("");
-    if property_find != PropertyType::Tel {
-	property = properties[0].get_value().to_string();
+        all.push_str(&format!(
+            "{}",
+            read_to_string(file.unwrap().path()).unwrap()
+        ));
     }
-    else {
-	property = properties[0].get_value().to_string().chars().filter(|c| c.is_digit(10)).collect();
-	}
-if property == value_find {
-	if let Some(property_find) = vcard.get_property_by_type(&property_show) {
-    		println!("{}", property_find.get_value()); }
-            	exit(0);
+    let vcards = parse_to_vcards_without_errors(&all);
+    for vcard in vcards {
+        let properties = vcard.get_properties_by_type(&property_find);
+        if !properties.is_empty() {
+            let mut property = String::from("");
+
+            if property_find != PropertyType::Tel {
+                property = properties[0].get_value().to_string();
+            } else {
+                property = properties[0]
+                    .get_value()
+                    .to_string()
+                    .chars()
+                    .filter(|c| c.is_digit(10))
+                    .collect();
             }
+
+            if property == value_find {
+                match property_show {
+                    PropertyType::Tel => {
+                        let properties_found = vcard.get_properties_by_type(&property_show);
+                        if !properties_found.is_empty() {
+                            let property_found: String = properties_found[0]
+                                .get_value()
+                                .to_string()
+                                .chars()
+                                .filter(|c| c.is_digit(10))
+                                .collect();
+                            println!("{}", property_found);
+                        }
+                    }
+                    PropertyType::Email => {
+                        let properties_found = vcard.get_properties_by_type(&property_show);
+                        if !properties_found.is_empty() {
+                            println!("{}", properties_found[0].get_value());
+                        }
+                    }
+
+                    _ => {
+                        if let Some(property_found) = vcard.get_property_by_type(&property_show) {
+                            println!("{}", property_found.get_value());
+                        }
+                    }
+                }
+                exit(0);
             }
         }
     }
